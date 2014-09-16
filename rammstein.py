c199f5b9222de5e60302e8359609df7985bfeb16
Willem Vooijs <willem.vooijs@avira.com>
2014-09-16 08:29:11 +0300

    Optimized for size

--- rammstein.py
+++ rammstein.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-lyrics = ['Du, du hast Mich gefragt und ich hab nichts gesagt', 'Willst du bis der Tod euch scheidet', 'Treu ihr sein für alle Tage?', 'Nein, nein!']
+lyrics = 'Du, du hast mich gefragt und ich hab nichts gesagt. Willst du bis der Tod euch scheidet Treu ihr sein für alle Tage ? Nein, nein!'
 
-def makeLine(lyric='', start=0, stop=False):
-    lyricParts = lyric.split(' ')
-    if stop == False:
-        stop = len(lyricParts)
-    return ' '.join(lyricParts[start:stop]).capitalize()
+def ucfirst(string):
+    output=string[0].upper()+string[1:]
+    return output
 
-for x in range(2):
-    print makeLine(lyrics[0], 0, 3)+'\n'+makeLine(lyrics[0], 1, 4)
-print
-for x in range(2):
-    for y in range(2):
-        print makeLine(lyrics[0], 0, 3)+'\n'+makeLine(lyrics[0], 1, 4)
-    print
-    print makeLine(lyrics[0], 0, 3)
-    for y in range(2):
-        print makeLine(lyrics[0], 1, 4)
-    print
-    for y in range(3):
-        print makeLine(lyrics[0], 1, 5)
-    print makeLine(lyrics[0], 5, 10)+'\n'
-    for y in range(2):
-        print makeLine(lyrics[1], 0)+'\n'+makeLine(lyrics[2], 0)+'\n'+makeLine(lyrics[3], 0)+'\n'
-print makeLine(lyrics[1], 0)+'\n'+makeLine(lyrics[2], 0)+'\n'+makeLine(lyrics[3], 0)
+a = lyrics[0:11]
+b = ucfirst(lyrics[4:16])
+ab = a+'\n'+b
+abab = ab+'\n'+ab+'\n'
+c = ucfirst(lyrics[4:24])
+d = ucfirst(lyrics[25:50])
+cd=c+'\n'+c+'\n'+c+'\n'+d+'\n'
+efg = lyrics[52:87]+'\n'+lyrics[88:118]+'\n'+lyrics[119:130]+'\n'
+ababcdefg = abab+'\n'+ab+'\n'+b+'\n'+'\n'+cd+'\n'+efg+'\n'+efg
+print abab+'\n'+ababcdefg+'\n'+ababcdefg+'\n'+efg
