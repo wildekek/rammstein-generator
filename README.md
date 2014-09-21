cd6f38c3d6834e9c8986c50ffcbf7a45f49fb28c
Willem Vooijs <willem.vooijs@avira.com>
2014-09-21 23:57:58 +0200

    optimized and corrected spelling in lyrics

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 rammstein-generator
 ===================
 
-This script generates the lyrics to Rammsteins 'Du Hast'. Feel free to optimize the code to be more compact, cryptic or otherwise but let's keep the SHA1 sum of the output to 7efc5cd96d4d0596007f0aaab820eb05bc746266, to verify the correctnes of the lyrics.
+This script generates the lyrics to Rammsteins 'Du Hast'. Feel free to optimize the code to be more compact, cryptic or otherwise but let's keep the SHA1 sum of the output to dc100b2675c5bacb3e062ada2d5de3c8a10a6a5d, to verify the correctnes of the lyrics.
 
 Copyright
 ---------
--- rammstein.py
+++ rammstein.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-lyrics = 'Du, du hast mich gefragt und ich hab nichts gesagt. Willst du bis der Tod euch scheidet Treu ihr sein für alle Tage ? Nein, nein!'
-
-def ucfirst(string):
-    output=string[0].upper()+string[1:]
-    return output
-
-a = lyrics[0:11]
-b = ucfirst(lyrics[4:16])
-ab = a+'\n'+b
-abab = ab+'\n'+ab+'\n'
-c = ucfirst(lyrics[4:24])
-d = ucfirst(lyrics[25:50])
-cd=c+'\n'+c+'\n'+c+'\n'+d+'\n'
-efg = lyrics[52:87]+'\n'+lyrics[88:118]+'\n'+lyrics[119:130]+'\n'
-ababcdefg = abab+'\n'+ab+'\n'+b+'\n'+'\n'+cd+'\n'+efg+'\n'+efg
-print abab+'\n'+ababcdefg+'\n'+ababcdefg+'\n'+efg
+a = 'Du hast mich'
+b = 'Du, du hast'+'\n'+a
+c = a+' gefragt'
+d = '\n'.join([b,b,''])
+e = '\n'.join(['Willst du bis der Tod euch scheidet','Treurig sein für alle Tage?','Nein, nein!',''])
+f = '\n'.join([d,b,a,'','\n'.join([c,c,c,'Und ich hab nichts gesagt','']),e,e])
+print '\n'.join([d,f,f,e])
