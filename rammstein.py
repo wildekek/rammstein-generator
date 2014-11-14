fc7ef34260c4dddaba01ff4c964349e13bd4bf1a
Willem Vooijs <willem.vooijs@avira.com>
2014-11-14 22:18:45 +0100

    Removed spaces and created function for joining

--- rammstein.py
+++ rammstein.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-a = 'Du hast mich'
-b = 'Du, du hast'+'\n'+a
-c = a+' gefragt'
-d = '\n'.join([b,b,''])
-e = '\n'.join(['Willst du bis der Tod euch scheidet','Treurig sein für alle Tage?','Nein, nein!',''])
-f = '\n'.join([d,b,a,'','\n'.join([c,c,c,'Und ich hab nichts gesagt','']),e,e])
-print '\n'.join([d,f,f,e])
+a='Du hast mich'
+b='Du, du hast'+'\n'+a
+c=a+' gefragt'
+def j(i):
+ return '\n'.join(i)
+d=j([b,b,''])
+e=j(['Willst du bis der Tod euch scheidet','Treurig sein für alle Tage?','Nein, nein!',''])
+f=j([d,b,a,'',j([c,c,c,'Und ich hab nichts gesagt','']),e,e])
+print j([d,f,f,e])
